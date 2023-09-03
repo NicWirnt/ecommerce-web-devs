0a66b803dec38a883903c2aa27a345d7bcbd5b10
NicWirnt <97414933+NicWirnt@users.noreply.github.com>
2023-09-03 13:52:12 +1000

    fix js

--- assets/js/header.js
+++ assets/js/header.js
@@ -19,7 +19,6 @@ const cart = document.querySelector(".cart-wrapper");
 document.querySelector("#cart").onclick = () => {
    
     cart.classList.toggle("active");
-    console.log(cart);
 }
 
 document.querySelector("#back-cart").onclick = () => {
