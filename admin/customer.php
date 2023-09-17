aa8f0a4ab16b7db976f49955de7892bee4526926
anhng <anananh1999@gmail.com>
2023-09-17 19:13:12 +1000

    View customers from admin side

---
+++ admin/customer.php
@@ -0,0 +1,66 @@
+<div class="grid gap-1" style="margin: 2rem 5rem;">
+    <p class="text-sky-400">Customers Management</p>
+<?php
+    
+    include("../config/connect.php");
+    $conn = OpenCon();
+    session_start();
+    if(isset($_SESSION['admin_id'])) {
+        include("index.php");
+        $Q = explode('=',$_SERVER['QUERY_STRING']);
+        $cus_id = $Q[1];
+        $select = $conn->prepare("SELECT * FROM `customers` JOIN `orders`ON orders.CustomerId = customers.ID WHERE `ID` = ? LIMIT 1;");
+        $select->execute([$cus_id]);
+        $row = $select->fetch(PDO::FETCH_ASSOC);
+        if($row == true) { ?>
+        <div class="max-w-ex p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <form>
+            <div class="relative z-0 w-full mb-6 group">
+            <input type="text" value="<?php echo htmlspecialchars($row['FirstName']);?>" id="id" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+            <label  class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">First Name</label>
+            </div>
+            <div class="relative z-0 w-full mb-6 group">
+                <input type="text" value="<?php echo htmlspecialchars($row['LastName']);?>" id="name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                <label class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Last Name</label>
+            </div>
+            <div class="relative z-0 w-full mb-6 group">
+                <input type="text" value="<?php echo htmlspecialchars($row['OrderNumber']);?>" id="name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                <label class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Order Number</label>
+            </div>
+            <div class="relative z-0 w-full mb-6 group">
+                <input type="text" value="<?php echo htmlspecialchars($row['OrderDate']);?>" id="name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                <label class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Order Date</label>
+            </div>
+            <div class="relative z-0 w-full mb-6 group">
+                <input type="text" value="<?php echo htmlspecialchars($row['ShipDate']);?>" id="name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                <label class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">ShipDate</label>
+            </div>
+            <div class="relative z-0 w-full mb-6 group">
+            <a href="/final/ecommerce-web-devs/admin/order_details.php?id=<?= $row['OrderId'];?>">
+                                <i class="fa fa-eye"></i> 
+                            </a>
+                <label style="margin: 10px;" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">View Orders</label>
+            </div>
+        </form>
+            
+        </div>
+        
+        <?php
+        } else {
+            echo 'This customer does not have any orders yet!!!';
+        }
+    } else{
+        echo '<h2>Need admin access</h2>';
+    };
+
+    CloseCon($conn);
+
+?>
+
+
+
+
+
+
+
+
---
+++ admin/customers.php
@@ -0,0 +1,55 @@
+<div class="grid gap-1" style="margin: 2rem 5rem;">
+    <p class="text-sky-400">Customers Management</p>
+
+<?php
+        include("../config/connect.php");
+        include("index.php");
+        $conn = OpenCon();
+        session_start();
+        if(isset($_SESSION['admin_id'])) {?>
+        <div class="relative overflow-x-auto">
+            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">Customer First Name</th>
+                        <th scope="col" class="px-6 py-3">Customer Last Name</th>
+                        <th scope="col" class="px-6 py-3">Email</th>
+                        <th scope="col" class="px-6 py-3">Address</th>
+                        <th scope="col" class="px-6 py-3">City</th>
+                        <th scope="col" class="px-6 py-3">Phone</th>
+                        <th scope="col" class="px-6 py-3">Postal Code</th>
+                        <th scope="col" class="px-6 py-3">View</th>
+                    </tr> 
+                </thead>  
+                <?php
+                $select = $conn->prepare("SELECT * FROM `customers`;");
+                $select->execute();
+                if($select->rowCount() > 0){ 
+                while($row = $select->fetch(PDO::FETCH_ASSOC)){ ?> 
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <td class="px-6 py-4"><?= $row['FirstName']; ?></td>
+                        <td class="px-6 py-4"><?= $row['LastName']; ?></td>
+                        <td class="px-6 py-4"><?= $row['Email']; ?></td>
+                        <td class="px-6 py-4"><?= $row['Address1']; ?></td>
+                        <td class="px-6 py-4"><?= $row['City']; ?></td>
+                        <td class="px-6 py-4"><?= $row['Phone']; ?></td>
+                        <td class="px-6 py-4"><?= $row['PostalCode']; ?></td>
+                        <td class="px-6 py-4">
+                            <a href="/final/ecommerce-web-devs/admin/customer.php?id=<?= $row['ID'];?>">
+                                <i class="fa fa-eye"></i> 
+                            </a>
+                            
+                        </td>
+                    </tr>
+        <?php } }else{
+                echo '<h2>No Category Found!</h2>';
+            } ?> 
+        </table> </div>
+        <?php 
+            } else{
+                echo '<h2>Need Admin Access</h2>';
+            }
+        CloseCon($conn);
+?>
+<p class="text-sky-400"> <a href="add_product.php">Add New Category</a></p>     
+</div>
